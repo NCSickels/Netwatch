82c52e10c52917c6ed2447792a75321bdf1bf92f
NCSickels <nsickels@trideum.com>
2023-11-13 12:23:03 -0600

    Fix help menu json file read error
    Fix for the helpMenu() function where the json file storing the table data would be null, causing a read error on function call.

--- src/netwatch.py
+++ src/netwatch.py
@@ -18,7 +18,8 @@ import os
 import configparser
 import time
 import json
-
+from rich.console import Console
+from rich.table import Table
 
 # Common Functions
 
@@ -62,10 +63,15 @@ class Utilities:
             i += 1
         print("\n")
 
+    def commandHistoryClear() -> None:
+        Utilities.command_history.clear()
+        print(f'\n{Color.OKGREEN}[✔] Command history cleared.{Color.END}\n')
+
     def endProgram() -> None:
-        print(" Finishing up...\n")
+        print("Finishing up...\n")
         with open(configFile, 'w') as configfile:
             config.write(configfile)
+        # TODO: Add terminal output save to log file here
         time.sleep(0.25)
         sys.exit()
 
@@ -110,10 +116,44 @@ class Utilities:
 
     def readJson(file_path: str) -> None:
         with open(file_path, 'r') as json_file:
-            json_data = json.load(json_file)
+            return json.load(json_file)
+            # jsonData = json.load(json_file)
+
+    def createTable(header_data: dict, column_data: list, row_data: list) -> None:
+        console = Console()
+        table = Table(
+            show_header=header_data.get("show_header", True),
+            header_style=header_data.get("header_style", "bold white"),
+            title=header_data.get("title", ""),
+            width=int(header_data.get("width", 84)),
+            show_lines=True)
+        for column in column_data:
+            table.add_column(
+                column.get("title", ""),
+                style=column.get("style", ""),
+                justify=column.get("justify", "right"),
+                width=column.get("width", 30))
+        column_names = [column.get("title", "").lower()
+                        for column in column_data]
+        for row in row_data:
+            row_values = [str(row.get(column_name, ""))
+                          for column_name in column_names]
+            table.add_row(*row_values)
+        return table
+
+    def helpMenu() -> None:
+        print("\n")
+        console = Console()
+        for tableName, tableData in jsonData.items():  # tableName, tableData
+            header_data = tableData[0]['header'][0]
+            column_data = tableData[0]['columns']
+            row_data = tableData[0]['rows']
+            table = Utilities.createTable(header_data, column_data, row_data)
+            console.print(table)
+            print("\n")
+
 
 # Start Menu Classes
-
 
 class Netwatch:
     netwatchLogo = '''
@@ -150,7 +190,7 @@ class Netwatch:
 '''
 
     def __init__(self):
-        # self.createFolders()
+        self.createFolders()
         choice = input(netwatchPrompt)
         Utilities.commandHistoryUpdate(choice)
         match choice:
@@ -158,19 +198,17 @@ class Netwatch:
                 print(InformationGathering.menuLogo)
                 InformationGathering()
             case "?" | "help":
-                pass
-                # Utilities.helpMenu()
-                # self.__init__()
+                Utilities.helpMenu()
+                self.__init__()
             case "update":
-                pass
-                # self.update()
+                self.update()
             case "\r" | "\n" | "" | " " | "back":
                 self.__init__()
             case "history":
                 Utilities.commandHistoryPrint()
                 self.__init__()
             case "history -c" | "history --clear":
-                Utilities.CommandHistoryClear()
+                Utilities.commandHistoryClear()
                 self.__init__()
             case "clear" | "cls":
                 Utilities.clearScr()
@@ -253,14 +291,17 @@ if __name__ == "__main__":
         installDir = os.path.dirname(os.path.abspath(__file__)) + '/'
         configFile = installDir + "/netwatch.cfg"
         print(installDir)
-        config = configparser.ConfigParser()
+        config = configparser.RawConfigParser()
         config.read(configFile)
         toolDir = installDir + config.get('netwatch', 'toolDir')
         logDir = installDir + config.get('netwatch', 'logDir')
         # yes = config.get('netwatch', 'yes').split()
 
-        file_path = installDir + "/main_helpmenu_table_data.json"
-        # json_data = Utilities.readJson(file_path)
+        file_path = installDir + "/mainHelpMenuData.json"
+        # if not os.path.exists(file_path):
+        #     print("Error here!")
+
+        jsonData = Utilities.readJson(file_path)
         # randomColor = [Color.HEADER, Color.IMPORTANT, Color.NOTICE, Color.BLUE, Color.OKBLUE,
         #            Color.OKGREEN, Color.WARNING, Color.RED, Color.END, Color.UNDERLINE, Color.BOLD, Color.LOGGING]
         # random.shuffle(randomColor)
